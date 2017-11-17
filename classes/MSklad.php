f0e378e30cca4e87bd4042677fa6b83ff8af0b08
kirill <ya.rafon-92@ya.ru>
2017-11-17 11:37:38 +0300

    Test

--- classes/MSklad.php
+++ classes/MSklad.php
@@ -1,19 +1,19 @@
 <?php
 class MSklad
 {
-    const username = "admin@magazen";
-    const password = "60552fad43";
+    const username = 'admin@magazen';
+    const password = '60552fad43';
     const salePriceId = 0;
-    const baseUrl = "https://online.moysklad.ru/api/remap/1.1";
+    const baseUrl = 'https://online.moysklad.ru/api/remap/1.1';
     private static $cached = [];
     public static function request($url, $isPost = false, $params = [])
     {
         try {
             if(!$isPost && !empty($params)) {
                 if(strpos($url, '?') > 0) {
-                    $url .= "&" . http_build_query($params);
+                    $url .= '&' . http_build_query($params);
                 } else {
-                    $url .= "?" . http_build_query($params);
+                    $url .= '?' . http_build_query($params);
                 }
             }
             if(isset(self::$cached[$url])) {
@@ -21,7 +21,7 @@ class MSklad
             }
             $curl = curl_init($url);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, ["Authorization: Basic " . base64_encode(self::username . ":" . self::password)]);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, ['Authorization: Basic ' . base64_encode(self::username . ':' . self::password)]);
             if($isPost) {
                 curl_setopt($curl, CURLOPT_POST, $isPost);
                 if (!empty($params)) {
@@ -38,67 +38,67 @@ class MSklad
         }
 
     }
-}
-
-class MSkladComponents extends MSklad {
-    const componentsUrl = MSklad::baseUrl . "/entity/bundle";
-    const productsUrl = MSklad::baseUrl . "/entity/product";
-    const variantsUrl = MSklad::baseUrl . "/entity/variant";
-
-    public static function findOffer($id) {
-        $data = simplexml_load_file($_SERVER['DOCUMENT_ROOT'].'/upload/1c_catalog/offers.xml');
-        $nodes = $data->xpath("//КоммерческаяИнформация/ПакетПредложений/Предложения/Предложение/Ид[contains(.,'{$id}')]/parent::*");
-        if(count($nodes) && isset($nodes[0])) {
-            return (array)$nodes[0];
-        }
-        return false;
-    }
-    public static function getComponents() {
-        $products = self::getAllProductsWithComponents();
+    public static function getAllPages($href, $params = ['offset' => 0, 'limit'=>100]) {
         $result = [];
-        foreach ($products as $product) {
-            foreach ($product['components']['rows'] as $row) {
-                $q = $row['quantity'];
-                $type = $row['assortment']['meta']['type'];
-                $href = $row['assortment']['meta']['href'];
-                $productArr = self::request($href);
-
-                if($type == "variant") {
-                    $father = self::findOffer($productArr['externalCode']);
-                    if($father) {
-                        $externalCode = $father['Ид'];
-                    } else {
-                        continue;
-                    }
-                } else {
-                    $externalCode = $productArr['externalCode'];
-                }
-                $price = $productArr['salePrices'][self::salePriceId]['value'] / 100;
-
-                if(!isset($result[$product['externalCode']])) {
-                    $result[$product['externalCode']] = [];
-                }
-                $result[$product['externalCode']][] = ['code' => $externalCode, 'quantity' => $q, 'price' => $price];
-            }
-        }
-        return $result;
-    }
-
-    public static function getAllProductsWithComponents() {
-        $products = [];
-        $href = self::componentsUrl;
-        $params = ['expand' => 'components', "offset" => 0, "limit"=>100];
         do {
-
             $res = MSklad::request($href, false, $params);
-            foreach ($res['rows'] as $row) {
-                if(isset($row['components'])) {
-                    $products[] = $row;
-                }
-            }
+            $result = array_merge($result, $res['rows']);
             $params = [];
             $href = isset($res['meta']['nextHref']) ? $res['meta']['nextHref'] : false;
         } while($href);
-        return $products;
+        return $result;
     }
 }
+
+class MSkladStock extends MSklad {
+    const url = MSklad::baseUrl . '/entity/store/';
+    public static function getStocks() {
+        return self::getAllPages(self::url, ['offset' => 0, 'limit'=>100]);
+    }
+    public static function getAll($id) {
+        return self::getAllPages(self::url.$id, ['offset' => 0, 'limit'=>100]);
+    }
+
+}
+class MSkladByStore extends MSklad {
+    const url = MSklad::baseUrl . '/report/stock/bystore';
+    public static function getAll() {
+        return self::getAllPages(self::url, ['offset' => 0, 'limit'=>100]);
+    }
+}
+class MSkladAssortment extends MSklad {
+    const url = MSklad::baseUrl . '/entity/assortment';
+    public static function getAll() {
+        return self::getAllPages(self::url, ['offset' => 0, 'limit'=>100]);
+    }
+}
+class MSkladProducts extends MSklad {
+    const url = MSklad::baseUrl . '/entity/product';
+    public static function getAll() {
+        return self::getAllPages(self::url, ['expand' => 'components', 'offset' => 0, 'limit'=>100]);
+    }
+}
+class MSkladOffers extends MSklad {
+    const url = MSklad::baseUrl . '/entity/variant';
+    public static function getAll() {
+        return self::getAllPages(self::url, ['offset' => 0, 'limit'=>100]);
+    }
+}
+$root = __DIR__;
+$pid = pcntl_fork();
+
+if(!$pid) {
+    $stores = MSkladByStore::getAll();
+    file_put_contents("{$root}/stores.txt", json_encode($stores));
+
+    $assortment = MSkladAssortment::getAll();
+    file_put_contents("{$root}/assortment.txt", json_encode($assortment));
+
+    $offers = MSkladOffers::getAll();
+    file_put_contents("{$root}/offers.txt", json_encode($offers));
+
+    $prods = MSkladProducts::getAll();
+    file_put_contents("{$root}/prods.txt", json_encode($prods));
+}
+
+
--- cron/exchange.php
+++ cron/exchange.php
@@ -1,2 +1,121 @@
 <?php
-require_once("../classes/MSklad.php");
+//require_once("../classes/MSklad.php");
+$usedStores = ['559a9eee-a865-11e7-7a69-8f550015ec59'];
+$stores = json_decode(file_get_contents("../classes/stores.txt"), true);
+$assortment = json_decode(file_get_contents("../classes/assortment.txt"), true);
+$offers = json_decode(file_get_contents("../classes/offers.txt"), true);
+$prods = json_decode(file_get_contents("../classes/prods.txt"), true);
+
+$arResult = [];
+foreach ($prods as $prod) {
+    if (isset($prod['salePrices']) && is_array($prod['salePrices'])) {
+        $price = $prod['salePrices'][0]['value'];
+    } else {
+        $price = false;
+    }
+    if (isset($prod['components'])) {
+        $components = $prod['components'];
+    } else {
+        $components = false;
+    }
+    if (isset($prod['code'])) {
+        $code = $prod['code'];
+    } else {
+        $code = false;
+    }
+    $arResult[$prod['id']] = [
+        'id' => $prod['id'],
+        'name' => $prod['name'],
+        'price' => $price,
+        'barcodes' => $prod['barcodes'],
+        'code' => $code,
+        'externalCode' => $prod['externalCode'],
+        'pathName' => $prod['pathName'],
+        'components' => $components,
+        'modificationsCount' => $prod['modificationsCount']
+    ];
+}
+$arOffers = [];
+$offersCharacteristics = [];
+foreach ($offers as $offer) {
+    if (isset($offer['salePrices']) && is_array($offer['salePrices'])) {
+        $price = $offer['salePrices'][0]['value'];
+    } else {
+        $price = false;
+    }
+
+    $characteristics = [];
+    foreach ($offer['characteristics'] as $characteristic) {
+        if (!isset($offersCharacteristics[$characteristic['id']])) {
+            $offersCharacteristics[$characteristic['id']] = [
+                'id' => $characteristic['id'],
+                'name' => $characteristic['name']
+            ];
+        }
+        $characteristics[$characteristic['id']] = $characteristic['value'];
+    }
+
+    if (isset($offer['product']) && isset($offer['product']['meta'])) {
+        $tmp = explode('/', $offer['product']['meta']['href']);
+        $prodId = array_pop($tmp);
+        $limit = strpos($prodId, '?');
+        if ($limit) {
+            $prodId = substr($prodId, 0, $prodId);
+        }
+    } else {
+        $prodId = null;
+    }
+
+    $arOffers[$offer['id']] = [
+        'id' => $offer['id'],
+        'name' => $offer['name'],
+        'price' => $price,
+        'barcodes' => $offer['barcodes'],
+        'code' => $offer['code'],
+        'externalCode' => $offer['externalCode'],
+        'prodId' => $prodId
+    ];
+};
+foreach ($stores as $store) {
+    $tmp = explode('/', $store['meta']['href']);
+    $chunk = array_pop($tmp);
+    $itemId = substr($chunk, 0, strpos($chunk, '?'));
+    $quantity = 0;
+    foreach ($store['stockByStore'] as $stock) {
+        $tmp = explode('/', $stock['meta']['href']);
+        $stockId = array_pop($tmp);
+        $limit =  strpos($stockId, '?');
+        if($limit) {
+            $stockId = substr($chunk, 0, $limit);
+        }
+        if (in_array($stockId, $usedStores)) {
+            $quantity += $stock['stock'];
+        }
+    }
+    switch ($store['meta']['type']) {
+        case 'product': {
+            if (isset($arResult[$itemId])) {
+                $arResult[$itemId]['quantity'] = $quantity;
+            } else {
+                echo $store['meta']['href'] . "\n";
+            }
+            break;
+        }
+        case 'variant' : {
+            if (isset($arOffers[$itemId])) {
+                $arOffers[$itemId]['quantity'] = $quantity;
+            } else {
+                echo $store['meta']['href'] . "\n";
+            }
+            break;
+        }
+    };
+}
+
+
+foreach ($arOffers as $offer) {
+    if ($offer['prodId'] && isset($arResult[$offer['prodId']])) {
+        $arOffers[$offer['id']]['product'] = $arResult[$offer['prodId']];
+    }
+}
+print_r($arOffers);
